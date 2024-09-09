382ded33e87d21cb22300c5fe23e020c6c2dfc92
Robert <oluwaseunrobert44@gmail.com>
2024-09-09 21:54:27 +0100

    COrrected origins

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server,  {
     cors: {
-        origin: ["http://localhost:3000", "https://eko-diary-nine.vercel.app"]
+        origins: ['http://localhost:3000', 'https://eko-diary-nine.vercel.app']
     }
 });
 
